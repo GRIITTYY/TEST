104418cafa128146df34b4600ff56153283fcfb0
GRIITTYY <iammomohsamuel@gmail.com>
2025-01-24 01:55:13 +0100

    Test Run

--- app2.py
+++ app2.py
@@ -33,7 +33,7 @@ def generate_qr_code(data):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
+        box_size=15,
         border=4,
     )
     qr.add_data(data)
